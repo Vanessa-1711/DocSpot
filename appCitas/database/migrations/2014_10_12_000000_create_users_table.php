c5a83c0ec39b0a66ef4cc4cca0d2adfc0313ca0d
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-10-30 10:23:23 -0600

    Campo imagen

--- appCitas/database/migrations/2014_10_12_000000_create_users_table.php
+++ appCitas/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
+            $table->string('imagen');
             $table->string('telefono');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- appCitas/vendor/composer/installed.php
+++ appCitas/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '35caaccce5323f89359ea47bf8897d2fae67215b',
+        'reference' => 'c346c6744efec3fc08242d41c75b2574e9b9d638',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -373,7 +373,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '35caaccce5323f89359ea47bf8897d2fae67215b',
+            'reference' => 'c346c6744efec3fc08242d41c75b2574e9b9d638',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
