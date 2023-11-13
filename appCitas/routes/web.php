c68d93e653d03f7f1bbdcbbe90f16926332289ab
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-11-13 11:49:32 -0600

    creacion del app blade inicial con el template

--- appCitas/routes/web.php
+++ appCitas/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [MainController::class, 'show']);
