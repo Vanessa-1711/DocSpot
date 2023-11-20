fb58bc1d8c675549a5c6f79e7d8b55f919900fe8
Nubia Cantú <2030210@upv.edu.mx>
2023-11-19 23:26:36 -0600

    login terminado

--- appCitas/app/Http/Controllers/LoginController.php
+++ appCitas/app/Http/Controllers/LoginController.php
@@ -8,6 +8,20 @@ class LoginController extends Controller
 {
     //
     public function store(Request $request){
-        dd('s');
+        $this->validate($request,[
+            // Reglas de validacion 
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        
+        // Verificar que las credenciales sean correctas
+        if(!auth()->attempt($request->only('username','password'), $request->remember)){
+            // Si las credenciales ingresadas no coinciden con las de la base de datos, retornar a la vista anterior con un mensaje de error y los valores anteriores
+            return back()->withErrors(['mensaje' => 'Las credenciales son incorrectas'])->withInput();
+        }
+        
+        // Credenciales correctas
+        return view('dashboard');
     }
+    
 }
