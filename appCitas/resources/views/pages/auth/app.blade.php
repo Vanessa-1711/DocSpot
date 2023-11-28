a9d024174aee35f1e60b98d43b020a9a5b3f2e65
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-11-28 07:17:42 -0600

    .

--- appCitas/resources/views/pages/auth/app.blade.php
+++ appCitas/resources/views/pages/auth/app.blade.php
@@ -27,7 +27,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
               <img src={{asset('img/logo-ct-dark.png')}}  class="navbar-brand-img h-100" alt="main_logo">
-              <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+              <span class="ms-1 font-weight-bold">DocSpot - Citas</span>
             </a>
           </div>
           <hr class="horizontal dark mt-0">
@@ -44,7 +44,12 @@
               <li class="nav-item">
                 <a class="nav-link " href="../pages/tables.html">
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+                    {{-- <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i> --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="200" height="200" fill="#00FFFF">
+                        <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-96 55.2C54 332.9 0 401.3 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7c0-81-54-149.4-128-171.1V362c27.6 7.1 48 
+                        32.2 48 62v40c0 8.8-7.2 16-16 16H336c-8.8 0-16-7.2-16-16s7.2-16 16-16V424c0-17.7-14.3-32-32-32s-32 14.3-32 32v24c8.8 0 16 7.2 16 16s-7.2 16-16 16H256c-8.8 0-16-7.2-16-16V424c0-29.8 20.4-54.9 
+                        48-62V304.9c-6-.6-12.1-.9-18.3-.9H178.3c-6.2 0-12.3 .3-18.3 .9v65.4c23.1 6.9 40 28.3 40 53.7c0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.4 16.9-46.8 40-53.7V311.2zM144 448a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
+                    </svg>
                   </div>
                   <span class="nav-link-text ms-1">Doctores</span>
                 </a>
@@ -52,7 +57,7 @@
               <li class="nav-item">
                 <a class="nav-link " href="../pages/billing.html">
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+                    <i class="ni ni-single-02 text-success text-sm opacity-10"></i>
                   </div>
                   <span class="nav-link-text ms-1">Pacientes</span>
                 </a>
@@ -60,7 +65,7 @@
               <li class="nav-item">
                 <a class="nav-link " href="../pages/virtual-reality.html">
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-app text-info text-sm opacity-10"></i>
+                    <i class="ni ni-calendar-grid-58 text-info text-sm opacity-10"></i>
                   </div>
                   <span class="nav-link-text ms-1">Citas</span>
                 </a>
@@ -68,7 +73,7 @@
               <li class="nav-item">
                 <a class="nav-link " href="../pages/rtl.html">
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
+                    <i class="ni ni-building text-danger text-sm opacity-10"></i>
                   </div>
                   <span class="nav-link-text ms-1">Hospitales</span>
                 </a>
@@ -574,9 +579,8 @@
                       © <script>
                         document.write(new Date().getFullYear())
                       </script>,
-                      made with <i class="fa fa-heart"></i> by
-                      <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                      for a better web.
+                      todos los derechos reservados <i class="fa fa-heart"></i> by
+                      <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">DocSpot</a>
                     </div>
                   </div>
                   <div class="col-lg-6">
