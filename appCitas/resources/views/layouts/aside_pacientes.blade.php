e4ac1e34af87e8ce4fdd909789fae6d04069265b
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-12-01 22:06:13 -0600

    menu, hospacientes view, etc

--- appCitas/resources/views/layouts/aside_pacientes.blade.php
+++ appCitas/resources/views/layouts/aside_pacientes.blade.php
@@ -7,7 +7,7 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-        <img src={{asset('img/logo-ct-dark.png')}}  class="navbar-brand-img h-100" alt="main_logo">
+        <img src={{asset('img/logos/hos.png')}}  class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 text-white font-weight-bold" style="font-size:22px">DocSpot</span>
       </a>
     </div>
@@ -15,7 +15,7 @@
     <div class="collapse navbar-collapse w-auto " id="sidenav-collapse-main" style="height:80% !important">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" href="../pages/dashboard.html">
+          <a class="nav-link active" href="{{route('paciente.dashboard')}}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-white text-sm opacity-10"></i>
             </div>
@@ -23,11 +23,11 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../pages/billing.html">
+          <a class="nav-link " href="{{ route('hospital.pacientes') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-white text-sm opacity-10"></i>
             </div>
-            <span class="text-white nav-link-text ms-1">Billing</span>
+            <span class="text-white nav-link-text ms-1">Hospitales</span>
           </a>
         </li>
       </ul>
