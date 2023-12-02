f74000565283ffca44a8bb5ecba5b536520d621b
Nubia Cantú <2030210@upv.edu.mx>
2023-12-01 20:38:06 -0600

    primera version menu

--- appCitas/routes/web.php
+++ appCitas/routes/web.php
@@ -32,6 +32,4 @@ Route::get('/medico/dashboard', [MedicoController::class, 'index'])->name('medic
 
 Route::get('/hospital/dashboard', [HospitalController::class, 'index'])->name('hospital.dashboard');
 
-Route::get('/app', function () {
-    return view('layouts.app');
-});
+
