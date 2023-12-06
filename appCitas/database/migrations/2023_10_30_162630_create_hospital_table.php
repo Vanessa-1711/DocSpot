7d9d2dd17c3444f887ccfcdb4c3642793ddfe0b1
Nubia Cantú <2030210@upv.edu.mx>
2023-12-05 22:21:24 -0600

    correccion migraciones

--- appCitas/database/migrations/2023_10_30_162630_create_hospital_table.php
+++ appCitas/database/migrations/2023_10_30_162630_create_hospital_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->double('latitud');
             $table->double('longitud');
             $table->string('tipo');
-            $table->string('user_id');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
