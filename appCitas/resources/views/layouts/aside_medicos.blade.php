9096edbebae009a9d29a7932c5516c3801c2d7c4
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-12-08 20:30:16 -0600

    corregir rutas aside DocSpot

--- appCitas/resources/views/layouts/aside_medicos.blade.php
+++ appCitas/resources/views/layouts/aside_medicos.blade.php
@@ -8,7 +8,7 @@
 <aside class="sidenav bg-primary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main" >
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0"  href="{{ route('hospital.pacientes') }}" target="_blank">
+      <a class="navbar-brand m-0"  href="{{ route('medico.dashboard') }}" target="_blank">
         <img src={{asset('img/logos/hos.png')}}  class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 text-white font-weight-bold" style="font-size:22px">DocSpot</span>
       </a>
