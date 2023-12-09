29ae281a312fb029800bb36bdcc7333d9d7a78df
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-12-08 20:12:45 -0600

    dashboard de medicos

--- appCitas/app/Http/Controllers/MedicoController.php
+++ appCitas/app/Http/Controllers/MedicoController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Citas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MedicoController extends Controller
 {
     public function index()
     {
-        return view('medico.dashboard');
+        $medicoId = Auth::user()->medico->id;
+
+        $citas = Citas::with('paciente')->where('medico_id', $medicoId)->get();
+        $totalCitas = Citas::where('medico_id', $medicoId)->count();
+        $citasPendientes = Citas::where('medico_id', $medicoId)
+                               ->where('fecha', '>', now())
+                               ->count();
+        $citasConfirmadas = Citas::where('medico_id', $medicoId)
+                                ->where('estado', 1)
+                                ->count();
+
+        return view('medico.dashboard', [
+            'citas' => $citas,
+            'totalCitas' => $totalCitas,
+            'citasPendientes' => $citasPendientes,
+            'citasConfirmadas' => $citasConfirmadas,
+        ]);
     }
 }
