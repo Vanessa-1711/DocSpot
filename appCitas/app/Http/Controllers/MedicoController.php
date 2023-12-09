8c8381d7a5835aae29956c7edfede7af6f97ac82
Nubia Cantú <2030210@upv.edu.mx>
2023-12-08 23:05:27 -0600

    pacientes en medicos

--- appCitas/app/Http/Controllers/MedicoController.php
+++ appCitas/app/Http/Controllers/MedicoController.php
@@ -31,4 +31,66 @@ class MedicoController extends Controller
             'citasConfirmadas' => $citasConfirmadas,
         ]);
     }
+
+    public function index_pacientes(){
+        $usuario = auth()->user();
+        $medico = Medico::where('user_id', $usuario->id)->first();
+        
+        if ($medico) { // Verificamos si se encontró al médico
+            $hospital = Hospital::find($medico->hospital_id); // Obtener el hospital del médico
+            
+            if ($hospital) {
+                $pacientesIds = PacienteHospital::where('hospital_id', $hospital->id)
+                    ->pluck('paciente_id')
+                    ->toArray();
+
+                $pacientes = Paciente::whereIn('id', $pacientesIds)->get();
+
+                return view('medico.pacientes', [
+                    'cantidadHospitales' => $pacientes->count(),
+                    'nombresPacientes' => $pacientes,
+                ]);
+
+            }
+        }
+    }
+
+    public function verMasPaciente($id)
+    {
+
+        $paciente = Paciente::find($id);
+        return view('medico.vermasPaciente', [
+            'paciente' => $paciente,
+           
+        ]);
+    }
+
+    public function crearCita($pacienteId)
+    
+    {
+        
+        $usuario = auth()->user();
+        $medico_id = Medico::where('user_id', $usuario->id)->first();
+    
+        // Encuentra al médico por su ID
+        $medico = Medico::findOrFail($medico_id->id);
+        
+        $paciente = Paciente::findOrFail($pacienteId);
+        
+
+        // Obtiene el ID del hospital del médico
+        $hospitalId = $medico->hospital_id;
+
+        $pacientesIds = PacienteHospital::where('hospital_id', $hospitalId)
+                    ->pluck('paciente_id')
+                    ->toArray();
+
+        $pacientes = Paciente::whereIn('id', $pacientesIds)->get();
+
+
+        return view('medico.crear_cita', [
+            'medico' => $paciente,
+            'medicosDelMismoHospital' => $pacientes
+        ]);
+    }
 }
